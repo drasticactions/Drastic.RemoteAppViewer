c22857dd81ad6e2e58756e4d9dc82a41572a16a2
Timothy Miller <innerlogic4321@gmail.com>
2022-10-09 13:55:00 +0900

    Initial Commit

---
+++ src/Drastic.RemoteAppViewer.VSMac/Properties/AddinInfo.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="AddinInfo.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "RemoteAppViewer",
+    Namespace = "Drastic",
+    Version = "99.0"
+)]
+
+[assembly: AddinName("Drastic Remote App Viewer")]
+[assembly: AddinCategory("Mobile Development")]
+[assembly: AddinDescription("Remote App Viewer")]
+[assembly: AddinAuthor("Drastic Actions")]
+
+static class VsBuildInfo
+{
+    // This is the minimum version of VS Mac that we require..
+    public const string Version = MonoDevelop.BuildInfo.Version;
+}
---
+++ src/Drastic.RemoteAppViewer.VSMac/RemoteAppViewerPad.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="RemoteAppViewerPad.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using MonoDevelop.Components;
+using MonoDevelop.Ide.Gui;
+
+namespace Drastic.RemoteAppViewer.VSMac
+{
+    public partial class RemoteAppViewerPad : PadContent
+    {
+        private NSView content;
+
+        public override Control Control => content;
+
+        public RemoteAppViewerPad()
+        {
+            this.content = new NSView();
+        }
+    }
+}
